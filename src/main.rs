9e0d8b5062fa255529a03595a335027a1bdff7b9
Q4kK <quantumstar4k@gmail.com>
2023-08-03 11:48:05 -0500

    mmmmmmmmmm

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,25 @@ use std::io;
 use std::path::*;
 
 fn main() {
-    let mut input = "";
     let home = match home_dir() {
         Some(home) => home,
         None => {
             panic!("No home directory found. oopsie daisy uh oh spaghettio");
         }
     };
-    println!("Enter a directory in which the project will go. (Default: ~/projects/)");
-    if io::stdin().read_line(&mut input).unwrap() = "" {
-        mkdir(&home.join("/projects/")).unwrap();
-    } else {
-        let path = Path::new(&input).to_path_buf();
-        mkdir()
-    }
+    let srcpath = home.join("projects/src/");
+    let binpath = home.join("projects/bin/");
+    println!("Project building in {:?}, {:?}", &srcpath, &binpath);
+    mkdir(&srcpath).unwrap();
+    mkdir(&binpath).unwrap();
 }
 
 fn mkdir(path: &Path) -> Result<(), io::Error> {
     println!("Building directory now.");
     fs::DirBuilder::new().recursive(true).create(path)
 }
+
+// fn make_project(proj_name: String) {
+//     srcpath = ""
+//     fs::File::create("{srcpath}/main");
+// }
