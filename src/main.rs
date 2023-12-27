cca4f6b7bb5989106b8d5510a1601e1ba655d9c5
Q4kK <quantumstar4k@gmail.com>
2023-12-27 13:18:11 -0600

    WIP: new workflow
    The idea is to have everything be inside of this directory, so that after starting a project you can enter a container where all the command line java stuff is streamlined into either a) a simple bashrc-type system where you could type `java project_name` and it would compile and test the program or b) an ongoing script that compiles and runs as you save the project

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
-use dirs::home_dir;
 use std::fs;
 use std::io;
 use std::path::*;
 
 fn main() {
-    let paths = ["src/", "bin/"];
-    let mut buffer = String::new();
+    let path = Path::new("./");
+    let mut input = String::new();
     let stdin = io::stdin();
-    let home = match home_dir() {
-        Some(home) => home.join("projects/"),
-        None => {
-            panic!("No home directory found. oopsie daisy uh oh spaghettio");
-        }
-    };
     println!("Hey, you should name the project:");
-    stdin.read_line(&mut buffer).unwrap();
-    let project_path = home.join(buffer.trim());
-    println!("Building Main.java in {}/src/", project_path.display());
-    for subfolder in paths {
-        make_directory(&project_path.join(subfolder)).unwrap();
+    stdin.read_line(&mut input).unwrap();
+    let project_path = path.join(input);
+
+    make_directory(&project_path).unwrap();
+    makeproject(&project_path);
+
+    fn make_directory(path: &Path) -> Result<(), io::Error> {
+        fs::DirBuilder::new().recursive(true).create(path)
     }
-    makeproject(&project_path.join("src/"));
-}
 
-fn make_directory(path: &Path) -> Result<(), io::Error> {
-    fs::DirBuilder::new().recursive(true).create(path)
-}
-
-fn makeproject(path: &Path) {
-    fs::write(
-        path.join("Main.java"),
-        "class Main {\n \n public static void main(String[] args) {\n \n} \n}",
-    )
-    .ok();
+    fn makeproject(path: &Path) {
+        fs::write(
+            path.join("Main.java"),
+            "class Main {\n \n public static void main(String[] args) {\n \n    } \n}",
+        )
+        .ok()
+        .unwrap();
+    }
 }
