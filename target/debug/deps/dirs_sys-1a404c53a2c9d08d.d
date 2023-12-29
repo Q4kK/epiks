7a0f92f7f7a179e1c8d9aae264e1e2abe25d5aa3
Q4kK <Quantumstar4k@gmail.com>
2023-12-29 10:14:00 -0600

    Feat: added .gitignore

--- target/debug/deps/dirs_sys-1a404c53a2c9d08d.d
+++
@@ -1,7 +0,0 @@
-/Users/hamiltonferris/projects/eclipse/epiks/target/debug/deps/dirs_sys-1a404c53a2c9d08d.rmeta: /Users/hamiltonferris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dirs-sys-0.4.1/src/lib.rs
-
-/Users/hamiltonferris/projects/eclipse/epiks/target/debug/deps/libdirs_sys-1a404c53a2c9d08d.rlib: /Users/hamiltonferris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dirs-sys-0.4.1/src/lib.rs
-
-/Users/hamiltonferris/projects/eclipse/epiks/target/debug/deps/dirs_sys-1a404c53a2c9d08d.d: /Users/hamiltonferris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dirs-sys-0.4.1/src/lib.rs
-
-/Users/hamiltonferris/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dirs-sys-0.4.1/src/lib.rs:
