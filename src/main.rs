dbb936042d75458e06a02854e88a809a2ec52938
q4kk <quantumstar4k@gmail.com>
2024-01-06 23:53:50 -0600

    WIP: read_dir comparing to path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::io;
+use std::path;
 use std::path::*;
-use std::Process::Command;
+use std::process::Command;
 
 fn main() {
     let path = Path::new("./");
@@ -10,10 +11,11 @@ fn main() {
     println!("Hey, you should name the project:");
     stdin.read_line(&mut input).unwrap();
     let project_path = path.join(input.trim());
+    let project_path_src = &project_path.join("src/");
 
-    make_directory(&project_path).unwrap();
-    makeproject(&project_path);
-
+    make_directory(&project_path).unwrap(); //make project path
+    make_directory(&project_path_src).unwrap(); //make project path with a src/ folder
+    makeproject(&project_path_src); //make Main.java in src/ folder
 }
 fn make_directory(path: &Path) -> Result<(), io::Error> {
     fs::DirBuilder::new().recursive(true).create(path)
@@ -29,17 +31,26 @@ fn makeproject(path: &Path) {
 }
 
 fn makeshell() {
-
     let shell_execute = if cfg!(target_os = "windows") {
-            // do this thing Command::new("")
-    } else if cfg!(target_os = "linux") {
-//do something
-    } else if cfg!(target_os = "macos") {
-//do something
-    }
-
+        // } else if cfg!(target_os = "linux") {
+        // } else if cfg!(target_os = "macos") {
+        // }
+    };
 }
+/*
 * contain nix flake that has java installed
 * launch a new terminal window with the old one running a bash shell
-* 
-/
+*
+*/
+
+fn check_for_dupe_dir(input_path: &Path) {
+    let dirs_read = fs::read_dir("./").unwrap();
+
+    for path in dirs_read {
+        if input_path == dirs_read. {
+            panic!("Dir name already taken!\n");
+        }
+    }
+}
+// make a funcion that checks against current existing directories
+// to see if the name of the directory conflicts
