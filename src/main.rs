d2fb0eacecab971173fa5ded2be3482a8b4ceed6
Q4kK <Quantumstar4k@gmail.com>
2024-01-10 10:50:50 -0600

    Feat: bin/ folder added
    It's way too messy for my liking, but I would rather have that then it not working lol

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,20 @@ fn main() {
     println!("Hey, you should name the project:");
     stdin.read_line(&mut input).unwrap();
     let project_path = path.join(input.trim());
-    let project_path_src = &project_path.join("src/");
 
     check_for_dupe_dir(&project_path);
-    make_directory(&project_path).unwrap(); //make project path
-    make_directory(&project_path_src).unwrap(); //make project path with a src/ folder
-    makeproject(&project_path_src); //make Main.java in src/ folder
+    make_directory(&project_path.join("src/"), &project_path.join("bin/")); //this is wayy too messy, need to fix later
+    makeproject(&project_path.join("src/")); //make Main.java in src/ folder
 }
-fn make_directory(path: &Path) -> Result<(), io::Error> {
-    fs::DirBuilder::new().recursive(true).create(path)
+fn make_directory(src_path: &Path, bin_path: &Path) {
+    fs::DirBuilder::new()
+        .recursive(true)
+        .create(src_path)
+        .unwrap();
+    fs::DirBuilder::new()
+        .recursive(true)
+        .create(bin_path)
+        .unwrap()
 }
 
 fn makeproject(path: &Path) {
@@ -31,19 +36,8 @@ fn makeproject(path: &Path) {
     .unwrap();
 }
 
-fn makeshell() {
-    let shell_execute = if cfg!(target_os = "windows") {
-        // } else if cfg!(target_os = "linux") {
-        // } else if cfg!(target_os = "macos") {
-        // }
-    };
-}
-/*
-* contain nix flake that has java installed
-* launch a new terminal window with the old one running a bash shell
-*
-*/
-
+// make a funcion that checks against current existing directories
+// to see if the name of the directory conflicts
 fn check_for_dupe_dir(input_path: &Path) {
     let dirs_read = fs::read_dir("./").unwrap();
 
@@ -53,5 +47,3 @@ fn check_for_dupe_dir(input_path: &Path) {
         }
     }
 }
-// make a funcion that checks against current existing directories
-// to see if the name of the directory conflicts
