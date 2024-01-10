d67584dbad15ce81dbdd8486a40db76114928e64
Q4kK <quantumstar4k@gmail.com>
2024-01-10 10:54:29 -0600

    Feat: duplicate directory checking & better code (#2)
    * WIP: read_dir comparing to path
    
    * Feat: check for duplicate directories
    
    * Feat: bin/ folder added
    
    It's way too messy for my liking, but I would rather have that then it not working lol

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::io;
+use std::path;
 use std::path::*;
-use std::Process::Command;
+use std::process::Command;
 
 fn main() {
     let path = Path::new("./");
@@ -11,12 +12,19 @@ fn main() {
     stdin.read_line(&mut input).unwrap();
     let project_path = path.join(input.trim());
 
-    make_directory(&project_path).unwrap();
-    makeproject(&project_path);
-
+    check_for_dupe_dir(&project_path);
+    make_directory(&project_path.join("src/"), &project_path.join("bin/")); //this is wayy too messy, need to fix later
+    makeproject(&project_path.join("src/")); //make Main.java in src/ folder
 }
-fn make_directory(path: &Path) -> Result<(), io::Error> {
-    fs::DirBuilder::new().recursive(true).create(path)
+fn make_directory(src_path: &Path, bin_path: &Path) {
+    fs::DirBuilder::new()
+        .recursive(true)
+        .create(src_path)
+        .unwrap();
+    fs::DirBuilder::new()
+        .recursive(true)
+        .create(bin_path)
+        .unwrap()
 }
 
 fn makeproject(path: &Path) {
@@ -28,18 +36,14 @@ fn makeproject(path: &Path) {
     .unwrap();
 }
 
-fn makeshell() {
+// make a funcion that checks against current existing directories
+// to see if the name of the directory conflicts
+fn check_for_dupe_dir(input_path: &Path) {
+    let dirs_read = fs::read_dir("./").unwrap();
 
-    let shell_execute = if cfg!(target_os = "windows") {
-            // do this thing Command::new("")
-    } else if cfg!(target_os = "linux") {
-//do something
-    } else if cfg!(target_os = "macos") {
-//do something
+    for path in dirs_read {
+        if input_path == path.unwrap().path() {
+            panic!("Dir name already taken!\n");
+        }
     }
-
 }
-* contain nix flake that has java installed
-* launch a new terminal window with the old one running a bash shell
-* 
-/
